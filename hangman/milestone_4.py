b77d132592940588ccf98c192814cc6065304e15
MaxThomas90 <56394971+MaxThomas90@users.noreply.github.com>
2023-11-27 14:37:34 +0000

    Completes task 2 of milestone 4.

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -9,24 +9,30 @@ class Hangman:
         self.word_guessed = ['_'] * len(self.word)
         self.num_letters = len(set(self.word))
         self.num_lives = num_lives
-        self.word_list = []
+        self.word_list = word_list
         self.list_of_guesses = []
 
-def check_guess(guess):
-    guess = guess.lower()
-    if guess in word:
-        print("Good guess! %s is in the word." % (guess))
-    else:
-        print("Sorry, %s is not in the word. Try again." % (guess))
-
-def ask_for_input():
-    while True:
-        guess = input("Choose a letter from the alphabet.")
-        if guess.isalpha() and len(guess)==1:
-            break
+    def check_guess(self, guess):
+        if guess in word:
+            print("Good guess! %s is in the word." % (guess))
         else:
-            print("Invalid letter. Please, enter a single alphabetical character.")
-    check_guess(guess)
+            print("Sorry, %s is not in the word. Try again." % (guess))
+
+    def ask_for_input(self):
+        while True:
+            guess = input("Choose a letter from the alphabet.").lower()
+            if not guess.isalpha() and len(guess)==1:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+                break
+
+
+
+            
 
 if __name__ == '__main__':
     word_list = ['mango', 'pear', 'apple', 'orange', 'banana']
@@ -34,10 +40,11 @@ if __name__ == '__main__':
     word = random.choice(word_list)
     print(word)
 
-    ask_for_input()
 
     game = Hangman(word_list)
-    print(game)
+    game.ask_for_input()
+
+
 
     
 

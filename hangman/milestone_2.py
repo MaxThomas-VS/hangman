9852d17705af961b2f06cbac50254af4bb17afc1
MaxThomas90 <56394971+MaxThomas90@users.noreply.github.com>
2023-11-27 13:31:37 +0000

    Completes task 2 for milestone 2.

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -2,6 +2,10 @@
 milestone 2 for hangman project
 '''
 
+import random
+
 if __name__ == '__main__':
     word_list = ['mango', 'pear', 'apple', 'orange', 'banana']
     print(word_list)
+    word = random.choice(word_list)
+    print(word)
